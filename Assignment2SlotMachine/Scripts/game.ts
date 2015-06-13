39a03ff06bbef8d3c4558609ba8786b1edd05526
guashtiSingh <gsingh540@my.centennialcollege.ca>
2015-06-13 15:07:14 -0400

    Buttons/imagesImplementedOnSlotMachine

--- Assignment2SlotMachine/Scripts/game.ts
+++ Assignment2SlotMachine/Scripts/game.ts
@@ -14,6 +14,8 @@
 // Game Framework Variables
 var canvas = document.getElementById("canvas");
 var stage: createjs.Stage;
+var tiles: createjs.Bitmap[] = [];
+var reelContainers: createjs.Container[] = [];
 var stats: Stats;
 
 var assets: createjs.LoadQueue;
@@ -60,12 +62,20 @@ var atlas = {
 
 };
 
+// GAME CONSTANTS
+var NUM_REELS: number = 3;
 
 // Game Variables
-var background: createjs.Bitmap;
-var textureAtlas: createjs.SpriteSheet;
-var spinButton: objects.Button;
-
+var winRatio = 0;
+var playerMoney = 1000;
+var winnings = 0;
+var jackpot = 5000;
+var turn = 0;
+var playerBet = 0;
+var winNumber = 0;
+var lossNumber = 0;
+var spinResult;
+var flower = "";
 
 //Tally Variables
 var flower1 = 0;
@@ -77,8 +87,39 @@ var flower6 = 0;
 var wild = 0;
 var blank = 0;
 
-var spinResult;
-var flower = "";
+//Game Objects
+var background: createjs.Bitmap;
+var textureAtlas: createjs.SpriteSheet;
+var spinButton: objects.Button;
+var resetButton: objects.Button;
+var betOneButton: objects.Button;
+var betTenButton: objects.Button;
+var powerButton: objects.Button;
+
+/* Utility function to reset all fruit tallies */
+function resetFruitTally() {
+    flower1 = 0;
+    flower2 = 0;
+    flower3 = 0;
+    flower4 = 0;
+    flower5 = 0;
+    flower6 = 0;
+    wild = 0;
+    blank = 0;
+}
+
+/* Utility function to reset the player stats */
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
 
 
 // Preloader Function
@@ -120,7 +161,6 @@ function setupStats() {
     document.body.appendChild(stats.domElement);
 }
 
-
 // Callback function that creates our Main Game Loop - refreshed 60 fps
 function gameLoop() {
     stats.begin(); // Begin measuring
@@ -136,6 +176,13 @@ function spinButtonClicked(event: createjs.MouseEvent) {
 
     spinResult = Reels();
     flower = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+
+    // Iterate over the number of reels
+    for (var index = 0; index < NUM_REELS; index++) {
+        reelContainers[index].removeAllChildren();
+        tiles[index] = new createjs.Bitmap("assets/images/" + spinResult[index] + ".png");
+        reelContainers[index].addChild(tiles[index]);
+    }
 }
 
 /* Utility function to check if a value falls within a range of bounds */
@@ -194,6 +241,67 @@ function Reels() {
     return betLine;
 }
 
+/* This function calculates the player's winnings, if any */
+function determineWinnings() {
+    if (blank == 0) {
+        if (flower1 == 3) {
+            winnings = playerBet * 10;
+        }
+        else if (flower2 == 3) {
+            winnings = playerBet * 20;
+        }
+        else if (flower3 == 3) {
+            winnings = playerBet * 30;
+        }
+        else if (flower4 == 3) {
+            winnings = playerBet * 40;
+        }
+        else if (flower5 == 3) {
+            winnings = playerBet * 50;
+        }
+        else if (flower6 == 3) {
+            winnings = playerBet * 75;
+        }
+        else if (wild == 3) {
+            winnings = playerBet * 100;
+        }
+        else if (flower1 == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (flower2 == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (flower3 == 2) {
+            winnings = playerBet * 3;
+        }
+        else if (flower4 == 2) {
+            winnings = playerBet * 4;
+        }
+        else if (flower5 == 2) {
+            winnings = playerBet * 5;
+        }
+        else if (flower6 == 2) {
+            winnings = playerBet * 10;
+        }
+        else if (wild == 2) {
+            winnings = playerBet * 20;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+
+        if (wild == 1) {
+            winnings = playerBet * 5;
+        }
+        winNumber++;
+        //showWinMessage();
+    }
+    else {
+        lossNumber++;
+        //showLossMessage();
+    }
+
+}
 
 
 // Callback functions that change the alpha transparency of the button
@@ -204,10 +312,38 @@ function main() {
     background = new createjs.Bitmap(assets.getResult("background"));
     stage.addChild(background);
 
+    for (var index = 0; index < NUM_REELS; index++) {
+        reelContainers[index] = new createjs.Container();
+        stage.addChild(reelContainers[index]);
+    }
+    reelContainers[0].x = 56;
+    reelContainers[0].y = 177;
+    reelContainers[1].x = 130;
+    reelContainers[1].y = 177;
+    reelContainers[2].x = 207;
+    reelContainers[2].y = 177;
+
     //Add spin button sprite
     spinButton = new objects.Button("spinButton", 236, 332, false);
     stage.addChild(spinButton);
     spinButton.on("click", spinButtonClicked, this);
 
+    //Add reset button sprite
+    resetButton = new objects.Button("resetButton", 35, 332, false);
+    stage.addChild(resetButton);
+    resetButton.on("click", spinButtonClicked, this);
 
+    //Add bet one button sprite
+    betOneButton = new objects.Button("betOneButton", 100, 332, false);
+    stage.addChild(betOneButton);
+    betOneButton.on("click", spinButtonClicked, this);
+
+    //Add bet ten button sprite
+    betTenButton = new objects.Button("betTenButton", 170, 332, false);
+    stage.addChild(betTenButton);
+    betTenButton.on("click", spinButtonClicked, this);
+
+    //Add power button sprite
+    powerButton = new objects.Button("powerButton", 241, 48, false);
+    stage.addChild(powerButton);
 }
