9c3711db35654ddec6ff28d1c865728d5ca7ebee
guashtiSingh <gsingh540@my.centennialcollege.ca>
2015-06-13 13:36:29 -0400

    spinButtonfunctionality

--- Assignment2SlotMachine/Scripts/game.ts
+++ Assignment2SlotMachine/Scripts/game.ts
@@ -67,6 +67,20 @@ var textureAtlas: createjs.SpriteSheet;
 var spinButton: objects.Button;
 
 
+//Tally Variables
+var flower1 = 0;
+var flower2 = 0;
+var flower3 = 0;
+var flower4 = 0;
+var flower5 = 0;
+var flower6 = 0;
+var wild = 0;
+var blank = 0;
+
+var spinResult;
+var flower = "";
+
+
 // Preloader Function
 function preload() {
     assets = new createjs.LoadQueue();
@@ -119,7 +133,68 @@ function gameLoop() {
 // Callback function that allows me to respond to button click events
 function spinButtonClicked(event: createjs.MouseEvent) {
     createjs.Sound.play("clicked");
+
+    spinResult = Reels();
+    flower = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
 }
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    }
+    else {
+        return !value;
+    }
+}
+
+/* When this function is called it determines the betLine results.
+e.g. Bar - Orange - Banana */
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                betLine[spin] = "blank";
+                blank++;
+                break;
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "Flower1";
+                flower1++;
+                break;
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "Flower2";
+                flower2++;
+                break;
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "Flower3";
+                flower3++;
+                break;
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "Flower4";
+                flower4++;
+                break;
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "Flower5";
+                flower5++;
+                break;
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "Flower6";
+                flower6++;
+                break;
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "Wild";
+                wild++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+
 
 // Callback functions that change the alpha transparency of the button
 
